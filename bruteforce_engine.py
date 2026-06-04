cb5bbc8813376cddf361f2159db8a7d89f4a49ed
blackcurrant404 <255106216+blackcurrant404@users.noreply.github.com>
2026-06-04 17:18:26 +0300

    store login results in engine using dictionary

--- bruteforce_engine.py
+++ bruteforce_engine.py
@@ -1,22 +1,35 @@
 import requests
+from datetime import datetime
 
 url = "http://127.0.0.1:5000/login"
 
 def engine(word_list: list, username_list: list):
+    result = {}
     for username in username_list:
+        result[username] = {
+            "bool": False,
+            "password": None,
+            "timestamp": None,
+            }
+            
         for password in word_list:
             payload = {
-                "username": "root",
+                "username": username,
                 "password": password
 
             }
             
             response = requests.post(url, data=payload)
             if is_login_successful(response):
-                print("FOUND")
-                break
+                result[username] = {
+                    "bool": True,
+                    "password": password,
+                    "timestamp": datetime.now()
+                    }
+                
+                break   
 
-    return None
+    return result
 
 def is_login_successful(response):
     return "successful" in response.text
