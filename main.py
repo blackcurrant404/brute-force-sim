54fad22609208488497495b7c522c46c5e3cf7e0
blackcurrant404 <255106216+blackcurrant404@users.noreply.github.com>
2026-06-04 22:59:36 +0300

    clean up code and fix minor issues

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from bruteforce_engine import engine
 from data_reader import reader
 from reporter import save_results_to_json
-from datetime import datetime
 
 def main():
     word_list, username_list = reader()
@@ -11,18 +10,3 @@ def main():
 if __name__ == "__main__":
 
     main()
-    if False:
-        save_results_to_json(
-            {
-                "pekka": {
-                    "bool": True,
-                    "password": "1234",
-                    "timestamp": datetime.now()
-                },
-                "lauri": {
-                    "bool": False,
-                    "password": "popo",
-                    "timestamp": datetime.now()
-                }
-                }
-        )
--- reporter.py
+++ reporter.py
@@ -1,8 +1,8 @@
 import json
 
 def save_results_to_json(result: dict):
-    result = serializer(result)
-    result_json = json.dumps(result, indent=4, sort_keys=True)
+    result_dict = serializer(result)
+    result_json = json.dumps(result_dict, indent=4, sort_keys=True)
     with open("logs/result.json", "w") as new_file:
         new_file.write(result_json)
     
